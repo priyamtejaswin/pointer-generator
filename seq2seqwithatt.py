7e0e25de526e18c4d6d61dd78aaa6d8a85531b93
Priyam Tejaswin <priyamtejaswin@gmail.com>
2020-12-19 05:23:35 -0500

    Baseline model implementation.

---
+++ seq2seqwithatt.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+"""
+created at: Sat 19 Dec 2020 05:22:17 AM EST
+created by: Priyam Tejaswin
+
+Implementing the (baseline) Seq2Seq+Attention model.
+References:
+Section 2 in <https://arxiv.org/pdf/1704.04368.pdf>
+"""
+
