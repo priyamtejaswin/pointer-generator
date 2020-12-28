64199cca0cd6382e02b8e37f38d08c8d032c6634
Priyam Tejaswin <priyamtejaswin@gmail.com>
2020-12-28 05:15:56 -0500

    Create tokenizer.

---
+++ create_tokenizer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""
+created at: Mon 28 Dec 2020 05:14:17 AM EST
+created by: Priyam Tejaswin (ptejaswi)
+
+Script to create the HugginFace WordPiece Tokenizer.
+!pip install tokenizers
+https://huggingface.co/docs/tokenizers/python/latest/quicktour.html
+"""
+
+
