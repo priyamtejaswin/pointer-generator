fbf04026a2ba8cd58c54ca0b3b8f05431f2eedaa
Priyam Tejaswin <priyamtejaswin@gmail.com>
2021-01-28 03:56:54 -0500

    New file for decoding codes.

---
+++ evaluate_checkpoint.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+created at: Thu 28 Jan 2021 03:55:04 AM EST
+created by: Priyam Tejaswin (ptejaswi)
+
+Script to score the model checkpoint.
+Usage:
+`python evaluate_checkpoint.py ckpts_dir ckpt-number`
+
+Script also implements Greedy and BeamSearch decoding.
+"""
+
